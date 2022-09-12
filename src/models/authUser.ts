2b40aaf0a724163bbc42ad629519ca4021d21286
t770113 <wallace.oliveira@santander.com.br>
2022-09-11 21:43:57 -0300

    feat: Model de response do NextAuth/Github

---
+++ src/models/authUser.ts
@@ -0,0 +1,11 @@
+export class AuthUser {
+  user: User = new User();
+  expires: string = "2022-10-12T00:33:44.686Z"
+}
+
+
+export class User {
+  name: string = "Wallace Wesley";
+  email: string = "wallace_wolv@hotmail.com";
+  image: string = "https://avatars.githubusercontent.com/u/65473975?v=4";
+}
