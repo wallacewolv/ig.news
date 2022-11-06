a5da40e902ad3a4b09efd8feffa89121495fbaa9
t770113 <wallace.oliveira@santander.com.br>
2022-11-05 22:12:10 -0300

    feat: Consuminado API do Prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,8 @@
+import Prismic from "@prismicio/client";
+import { GetStaticProps } from "next";
 import Head from "next/head";
+
+import { getPrismicCLient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 
 export default function Posts() {
@@ -39,3 +43,21 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicCLient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "publication")],
+    {
+      fetch: ["publication.title", "publication.content"],
+      pageSize: 100,
+    }
+  );
+
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {},
+  };
+};
